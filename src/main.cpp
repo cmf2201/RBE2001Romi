62d6cded395d4feec75cfb04eea18243a6696ba9
kracka731 <kracka@wpi.edu>
2023-03-01 18:36:47 -0500

    linear gripper is estoppable

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,6 @@ Romi32U4ButtonC buttonC;
 
 RemoteControl remoteControl(2);
 
-#define BOTTOM_OUT_GRIPPER true
-
 Servo32U4Pin5 gripperServo;
 Servo32U4 jawServo;
 
@@ -31,6 +29,7 @@ QTRSensors qtr;
 const uint8_t SensorCount = 2;
 uint16_t sensorValues[SensorCount];
 
+bool BOTTOM_OUT_GRIPPER = true;
 
 //Linear Servo declarations
 int servoPin = 0;
@@ -54,10 +53,10 @@ int servoEnc = A0;
 
 bool servoActive = false;
 
-int servoClosedPositionMS = 1250;
-int servoClosedPositionAR = 246;
-int servoOpenedPositionMS = 10;
-int servoOpenedPositionAR = 137;
+int servoClosedPositionMS = 2000; // og = 1250
+int servoClosedPositionAR = 390;
+int servoOpenedPositionMS = 1000; // 275? og =10
+int servoOpenedPositionAR = 205; // og 137
 
 int servoStuckTolerance = 4;
 int servoCloseTolerance = 5;
@@ -90,6 +89,10 @@ float CPR = 270;
 int motorEffort = 400;
 int deadBandCurrentEffort = 0;
 
+// motor positions for bottom out gripper robot
+int bottomOutSpot45 = 3472;
+int bottomOutSpot60 = 8500;
+
 // Ultrasonic Sensor Variables 
 const byte ultrasonicTriggerPin = 3;
 const byte ultrasonicEchoPin = 30;
@@ -101,8 +104,8 @@ long ultrasonicSignalDuration = 0;
 
 bool paused = false;
 
-void moveUp(void);
-void moveDown(void);
+// void moveUp(void);
+// void moveDown(void);
 
 void stopIt() {
   Serial.println("ESTOP");
@@ -123,16 +126,24 @@ void printTest() {
   Serial.println(analogRead(A4));
 }
 
-void closeServo() {
-  Serial.println(analogRead(linearPotPin));
-  gripperServo.writeMicroseconds(2000);
-}
+// void closeServo() {
+//   Serial.println(analogRead(linearPotPin));
+//   gripperServo.writeMicroseconds(2000);
+// }
 
-void openServo() {
-  Serial.println(analogRead(linearPotPin));
-  gripperServo.writeMicroseconds(10);
-}
+// void openServo() {
+//   Serial.println(analogRead(linearPotPin));
+//   gripperServo.writeMicroseconds(10);
+// }
 
+void changeGripperType() {
+  BOTTOM_OUT_GRIPPER = !BOTTOM_OUT_GRIPPER;
+  if (BOTTOM_OUT_GRIPPER) {
+    Serial.println("Bottom Out Gripper is now active");
+  } else {
+    Serial.println("Linear Gripper is now active");
+  }
+}
 
 void ultrasonicDistance() {
   digitalWrite(ultrasonicTriggerPin, LOW); // Clear ultrasonic trigger pin
@@ -286,24 +297,47 @@ void batteryCheck() {
   if (readBatteryMillivolts() <= 6000) {
     Serial.print("BATTERY LOW: ");
     Serial.println(readBatteryMillivolts());
-    buttonC.waitForButton();
+    // buttonC.waitForButton();
   }
 }
 
 //opens the bottom out gripper
 void openBottomGripper() {
+  Serial.println("open going");
   if(servoActive) {
     int servoCurrentPosition = analogRead(servoEnc);
     Serial.println(servoCurrentPosition);
-    gripperServo.writeMicroseconds(servoOpenedPositionMS);
-    servoActive = false;
-    Serial.println("OPENING");
+    if(servoCurrentPosition > servoOpenedPositionAR) {
+      Serial.println(servoCurrentPosition);
+      gripperServo.writeMicroseconds(servoOpenedPositionMS);
+      Serial.println("OPENING");
+      servoCurrentPosition = analogRead(servoEnc);
+    } else {
+      servoActive = false;
+      Serial.println("OVER");
+      remoteControl.stopFunction(remote2);
+    }
   }
 }
 
 //closes the bottom out gripper, but stops if the gripper cannot close
 void closeBottomGripper() {
-  if(servoActive) {
+  // Serial.println("close going");
+  // if(servoActive) {
+  //   int servoCurrentPosition = analogRead(servoEnc);
+  //   Serial.println(servoCurrentPosition);
+  //   if (servoCurrentPosition < servoClosedPositionAR) {
+  //     Serial.println(servoCurrentPosition);
+  //     gripperServo.writeMicroseconds(servoClosedPositionMS);
+  //     Serial.println("CLOSING");
+  //     servoCurrentPosition = analogRead(servoEnc);
+  //   } else {
+  //     servoActive = false;
+  //     Serial.println("OVER");
+  //     remoteControl.stopFunction(remote1);
+  //   }
+  // }
+
     int servoCurrentPosition = analogRead(servoEnc);
     Serial.println(servoCurrentPosition);
 
@@ -317,6 +351,7 @@ void closeBottomGripper() {
         if(servoStillCount >= 15) {
           Serial.println("STUCK!");
           openBottomGripper();
+          remoteControl.stopFunction(remote1);
         }
         servoStillCount++;
         
@@ -328,12 +363,13 @@ void closeBottomGripper() {
       if(abs(servoCurrentPosition - servoClosedPositionAR) <= servoCloseTolerance) {
         Serial.println("FINISHED!");
         servoActive = false;
+        remoteControl.stopFunction(remote1);
       }
 
       servoPrevReading = servoCurrentPosition;
       servoPrevMS = millis();
     }
-  }
+  
 
 }
 
@@ -353,6 +389,11 @@ void bottomOutGripper() {
 
 // Allows testing of Linear Gripper functionality, stopping and opening if servo is stuck, and closing onto plate on B press.
 void openGripper() {
+
+  if(BOTTOM_OUT_GRIPPER) {
+    servoActive = true;
+    openBottomGripper();
+  } else {
   // Get Pot Value
   linearPotVoltageADC = analogRead(linearPotPin);
   Serial.print("Initial linearPotVoltageADC:   ");
@@ -372,12 +413,19 @@ void openGripper() {
     jawServo.writeMicroseconds(servoStop);
     remoteControl.stopFunction(remote2);
     }
+  }
 }
 
 void closeGripper() {
+  Serial.println("close button pressed");  
+  if (BOTTOM_OUT_GRIPPER) {
+    servoActive = true;
+    Serial.println("close gripper going");
+    closeBottomGripper(); 
+  } else {
     linearPotVoltageADC = analogRead(linearPotPin);
-  Serial.print("Initial linearPotVoltageADC:   ");
-  Serial.println(linearPotVoltageADC); 
+    Serial.print("Initial linearPotVoltageADC:   ");
+    Serial.println(linearPotVoltageADC); 
       // // Move Jaw Up
      jawServo.writeMicroseconds(servoJawUp);
 
@@ -395,10 +443,14 @@ void closeGripper() {
     jawServo.writeMicroseconds(servoStop);
     remoteControl.stopFunction(remote1);
     }
+  }
 }
 
 // Moves motor until 4-bar linkage and gripper are positioned to grab the 60 degree plate
 void takeOffHighPlate() {
+  if(BOTTOM_OUT_GRIPPER) {
+    firstSpot60deg = bottomOutSpot60;
+  }
   if(motor.getToggleOff()) {
     motor.moveTo(firstSpot60deg);
   } else {
@@ -409,6 +461,9 @@ void takeOffHighPlate() {
 
 // Moves motor until 4-bar linkage and gripper are positioned to grab the 60 degree plate
 void takeOffLowPlate() {
+  if(BOTTOM_OUT_GRIPPER) {
+    firstSpot45deg = bottomOutSpot45;
+  }
   if(motor.getToggleOff()) {
     motor.moveTo(firstSpot45deg);
   } else {
@@ -459,6 +514,7 @@ void resetMode() {
 
 void resetEncoder() {
   motor.reset();
+  Serial.println("Blue motor reset");
 }
 
 void setup()
@@ -477,10 +533,15 @@ void setup()
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){A4, A3}, SensorCount);
 
+  pinMode(A0,INPUT);
+
   pinMode(ultrasonicEchoPin,INPUT); 
   pinMode(ultrasonicTriggerPin, OUTPUT);
 
   remoteControl.setup();
+
+  remoteControl.onPress(changeGripperType, remoteSetup);
+  remoteControl.onPress(batteryCheck, remotePlayPause);
 
   remoteControl.onPress(moveUp,remoteUp);
   remoteControl.onPress(moveDown,remoteDown);
@@ -530,6 +591,20 @@ void loop()
   // goToPositions();
 
   remoteControl.checkRemoteButtons();
+
+  // gripperServo.writeMicroseconds(servoClosedPositionMS);
+  // delay(500);
+  // Serial.println("1");
+  //  Serial.println(analogRead(A0));
+  // delay(500);
+  // gripperServo.writeMicroseconds(servoOpenedPositionMS);
+  // delay(500);
+  // Serial.println("2");
+  // Serial.println(analogRead(A0));
+  // delay(500);
+  // Serial.println("3");
+  // gripperServo.writeMicroseconds(1000);
+
 
   // motor.setEffort(200);
 //
